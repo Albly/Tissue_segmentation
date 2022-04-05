657514f519983a9cbc7897be9c4fa074dd164add
Skrikunov <krikunov.stanis@gmail.com>
2022-04-05 22:11:09 +0300

    IoU+Augmentations

--- loss.py
+++ loss.py
@@ -1,6 +1,6 @@
 import torch
 
-def calc_val_data(TR,PR):
+def calc_val_data_dice(TR,PR):
     """
     Calculating pixel-wise validation metrics 
     TR = true mask
@@ -13,10 +13,32 @@ def calc_val_data(TR,PR):
     FN = (PR == class_lables[0]) & (TR != PR)
     return TP,TN,FP,FN
 
-def calc_val_loss(TP,TN,FP,FN, eps = 1e-7):
+def calc_val_loss_dice(TP,TN,FP,FN,eps=1e-8):
     """
     Dice score calculating
     """
     tp,tn,fp,fn = TP.sum(),TN.sum(),FP.sum(),FN.sum()
-    dice_score = 2*tp / (2*tp+fp+fn+eps)
+    dice_score = 2*tp / (2*tp+fp+fn + eps)
     return dice_score
+
+def calc_val_data_IoU(TR,PR):
+    """
+    Calculating of intersection,union and target
+    """
+    class_lables = [0,255]
+    I = torch.zeros([2])
+    U = torch.zeros([2])
+    T = torch.zeros([2])
+    for i,j in enumerate(class_lables):
+        I[i] = ((TR == j) & (PR == j)).sum()
+        U[i] = ((TR == j) | (PR == j)).sum()
+        T[i] = (TR == j).sum()
+    return I,U,T
+
+def calc_val_loss_IoU(I,U,T,eps=1e-8):
+    """
+    IoU and accuracy calculating
+    """
+    IoU = I/(U + eps)
+    Acc = I/(T + eps)
+    return IoU,Acc
