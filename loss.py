532094cf4304eab206414f50a6c9bb35532504b8
Skrikunov <krikunov.stanis@gmail.com>
2022-04-05 18:47:11 +0300

    dice loss

--- loss.py
+++ loss.py
@@ -1,3 +1,5 @@
+import torch
+
 def calc_val_data(TR,PR):
     """
     Calculating pixel-wise validation metrics 
@@ -18,13 +20,3 @@ def calc_val_loss(TP,TN,FP,FN, eps = 1e-7):
     tp,tn,fp,fn = TP.sum(),TN.sum(),FP.sum(),FN.sum()
     dice_score = 2*tp / (2*tp+fp+fn+eps)
     return dice_score
-
-# size = 3
-# TR = torch.randint(0,2,[size,size],dtype=torch.uint8)*255
-# PR = torch.randint(0,2,[size,size],dtype=torch.uint8)*255
-
-# TP,TN,FP,FN = calc_val_data(TR, PR)
-# dice_score = calc_val_loss(TP,TN,FP,FN,eps=1e-7)
-# print(TR)
-# print(PR)
-# TP,TN,FP,FN
